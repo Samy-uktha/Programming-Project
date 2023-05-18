c9a453768118156b01373e677717a68f99676035
Samy-uktha <132467295+Samy-uktha@users.noreply.github.com>
2023-05-18 16:37:24 +0530

    Add files via upload

--- project2.py
+++ project2.py
@@ -15,6 +15,7 @@ class ShoppingCart:
 
 def add_item_to_cart(item):
     cart.add_item(item)
+    clicked_items.insert(tk.END, item["name"])
     print(f"{item['name']} added to the cart.")
     update_total_price()
 
@@ -27,21 +28,32 @@ def main():
     cart = ShoppingCart()
 
     root = tk.Tk()
-    root.title("Shopping Cart")
-    root.geometry("1600x750")
+    root.title("Grocery Store")
+    root.geometry("1000x750")
 
     def on_button_click(item):
         add_item_to_cart(item)
 
     # Create buttons for adding items to the cart
     items = [
-        {"name": "1kg apples", "price": 10.99},
-        {"name": "1kg bananas", "price": 5.99},
-        {"name": "1kg grapes", "price": 7.99}
+        {"name": "1kg apple", "price": 10.99},
+        {"name": "1kg banana", "price": 5.99},
+        {"name": "1kg grape", "price": 7.99},
+        {"name": "1kg mango", "price": 15.99},
+        {"name": "1kg watermelon", "price": 15.99},
+        {"name": "1kg papaya", "price": 15.99},
+        {"name": "1kg jackfruit", "price": 15.99},
     ]
     for item in items:
         button = tk.Button(root, text=f"{item['name']} (${item['price']:.2f})", command=lambda item=item: on_button_click(item))
         button.pack()
+    
+    clicked_items_label = tk.Label(root, text="Shopping Cart:", background="pink", border=5)
+    clicked_items_label.pack()
+    
+    global clicked_items
+    clicked_items = tk.Listbox(root, background="pink")
+    clicked_items.pack()
 
     global price_label
     price_label = tk.Label(root, text="Total Price: $0.00")
